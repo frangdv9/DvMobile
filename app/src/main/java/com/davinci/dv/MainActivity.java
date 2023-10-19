2575381f55bae026ae7beb0cf5d9a1f5d73ba6d7
fran <frangomez0409@gmail.com>
2023-10-19 11:59:42 -0300

    feat: add crear publicacion (sin imagen debido a que el dispositivo fisico desconoce la direccion)

--- app/src/main/java/com/davinci/dv/MainActivity.java
+++ app/src/main/java/com/davinci/dv/MainActivity.java
@@ -2,7 +2,13 @@ package com.davinci.dv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn = findViewById(R.id.button_texto);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button) {
+                LinearLayout scrolleable = findViewById(R.id.scrolleable);
+                    LinearLayout nuevoPost = new LinearLayout(button.getContext());
+                    //ImageView imagenNuevoPost = new ImageView(button.getContext());
+                    TextView textoNuevoPost = new TextView(button.getContext());
+                    nuevoPost.setOrientation(LinearLayout.VERTICAL);
+                    //imagenNuevoPost.setBackground(
+                     //       getDrawable(R.drawable.ic_launcher_foreground));
+                    textoNuevoPost.setText("Nueva Publi");
+
+                    //nuevoPost.addView(imagenNuevoPost);
+                    nuevoPost.addView(textoNuevoPost);
+                scrolleable.addView(nuevoPost);
+            }
+
+        });
+
     }
 }
